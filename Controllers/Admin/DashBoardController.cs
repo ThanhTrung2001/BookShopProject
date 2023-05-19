f633b5f5734a5ddee7688b53fab9f5a691255dd9
V.Kohaku <thanhtrungtran13@gmail.com>
2023-05-19 12:00:20 +0700

    frame for dashboard, index page

--- Controllers/Admin/DashBoardController.cs
+++ Controllers/Admin/DashBoardController.cs
@@ -2,17 +2,23 @@
 
 namespace BookShopProject.Controllers
 {
-  public class DashBoardController : Controller
-  {
-    public IActionResult Index()
+    public class DashBoardController : Controller
     {
-      return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [HttpGet]
-    public IActionResult Details()
-    {
-      return View();
+        [HttpGet]
+        public IActionResult Information()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LogOut()
+        {
+            return View();
+        }
     }
-  }
 }
